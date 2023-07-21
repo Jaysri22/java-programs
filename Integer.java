55c7b22e0e2dff53b83439d4bf0d2a0441a2ddd7
Jaysri22 <124653118+Jaysri22@users.noreply.github.com>
2023-07-21 01:48:40 -0700

    Add files via upload

---
+++ Integer.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class Integer
+{
+char c,x,d;
+int i,s;
+scanner sc=new scanner(System.in);
+System.out.println("enter the string ");
+string a=sc.nextline();
+int a=a.length();
+System.out.println(n);
+for(i=0;i<n;i++)
+{
+a=a.charAt(i);
+d=a.charAt(n-1-i);
+s=(int)c+(int)d-96;
+x=(char)s;
+System.out.print(x);
+}
+}
+
+		
