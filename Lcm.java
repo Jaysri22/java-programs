49f8a022884bb9b270388cfd2424559e2603a23a
Jaysri22 <124653118+Jaysri22@users.noreply.github.com>
2023-07-21 01:57:54 -0700

    Add files via upload

---
+++ Lcm.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Lcm{
+public static void main(String args[]){
+int n1=40;
+int n2=50;
+int num;
+if(num%n1==0 &&num%n2==0)
+{
+System.out.println("lcm of two numbers is"= +num);
+}
+else
+{
+System.out.println("no");
+}}}
