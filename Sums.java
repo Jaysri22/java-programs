6e13d381d08b2e01d99011089be02d234e09e580
Jaysri22 <124653118+Jaysri22@users.noreply.github.com>
2023-07-21 01:50:58 -0700

    Add files via upload

---
+++ Sums.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Sums{
+public static void main(String args[]){
+int a[]={1,2,3,4,5};
+int n,i,avg;
+int sum=1;
+n=a.length;
+for(i=0;i<n;i++)
+{
+sum=sum*a[i];}
+//avg=sum/n;
+System.out.println(sum);
+}}
