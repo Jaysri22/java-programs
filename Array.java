9b54d96b3f700e51caf0d63b479b59360d4f455a
Jaysri22 <124653118+Jaysri22@users.noreply.github.com>
2023-07-21 01:52:06 -0700

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.util.Arrays;
+class Array{
+public static void main(String args[]){
+int a[]={1,2,3,4,5};
+int n;
+n=a.length;
+System.out.println(Arrays.toString(a));
+}}
