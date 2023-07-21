fae310555d6fea013adc091ca6a4779d7cedbb5d
Jaysri22 <124653118+Jaysri22@users.noreply.github.com>
2023-07-21 01:56:35 -0700

    Add files via upload

---
+++ One.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class One{
+public static void main(String args[]){
+int n;
+System.out.println("enter a no");
+Scanner s =new Scanner(System.in);
+n=s.nextInt();
+int i,j;
+for(j=1;j<=n;j++)
+	
+	{
+for(i=1;i<=j;i++)
+{
+
+
+System.out.print("i");}
+
+System.out.println(" ");
+}}}
