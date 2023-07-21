e857e11660d23a1af56d4dad4f19ae8f7e25cbd1
Jaysri22 <124653118+Jaysri22@users.noreply.github.com>
2023-07-21 01:46:30 -0700

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Square{
+public static void main(String[]args){
+
+Scanner s=new Scanner(System.in);
+
+System.out.println("enter the value of sides");
+int l=s.nextInt();
+
+int c=l*l;
+System.out.println("area of square is:" +c);
+}
+}
